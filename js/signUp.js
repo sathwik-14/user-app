1f0a3d5e058764167d524d39f8bd200a36bd43a2
Wagle Sathwik <4VP19CS080@vcetputtur.ac.in>
2023-05-05 14:10:03 +0530

    Update signUp.js

--- js/signUp.js
+++ js/signUp.js
@@ -104,7 +104,7 @@ import {getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword,init
           alert("success")
           setTimeout(()=>{
             const link = document.createElement('a');
-    link.href = '/user-app/index.html';
+    link.href = '/';
     link.click()
     
           }
